68cb11f2120cb6a9c1eefd65d79294cb087c9bef
Manvith Kumar <kumarmanvith0@gmail.com>
2025-03-01 13:41:56 +0530

    new change for db

--- rms-node-server/config/database.js
+++ rms-node-server/config/database.js
@@ -1,11 +1,13 @@
 const mysql = require("mysql2");
 
+require('dotenv').config();
+
 // Create a connection pool (recommended for performance)
 const db = mysql.createPool({
-  host: "localhost",      // Change this if using a remote MySQL server
-  user: "root",           // Your MySQL username
-  password: "",           // Your MySQL password (keep empty if no password)
-  database: "rms",        // Your MySQL database name
+  host: process.env.DATABASE_HOST,      // Change this if using a remote MySQL server
+  user: process.env.DATABASE_USER,           // Your MySQL username
+  password: process.env.DATABASE_PASSWORD,           // Your MySQL password (keep empty if no password)
+  database: process.env.DATABASE_DB,        // Your MySQL database name
   waitForConnections: true,
   connectionLimit: 10,    // Maximum number of connections
   queueLimit: 0
